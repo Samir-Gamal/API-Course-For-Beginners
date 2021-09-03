804267865da509d3299b9a855d0f9cf7cc1d8b32
John Doe <69224131+Samir-Gamal@users.noreply.github.com>
2021-09-03 23:45:49 +0300

    api resource naming

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -11,19 +12,21 @@ class PostController extends Controller
     use ApiResponseTrait;
 
     public function index(){
-        $posts = Post::get();
+
+        $posts = PostResource::collection(Post::get());
         return $this->apiResponse($posts,'ok',200);
     }
 
     public function show($id){
 
-        $post = Post::find($id);
+        $post = new PostResource(Post::find($id));
 
         if($post){
             return $this->apiResponse($post,'ok',200);
         }
 
-        return $this->apiResponse(null,'The Post Not Found',401);
+        return $this->apiResponse(null,'The post Not Found',404);
+
 
     }
 }
