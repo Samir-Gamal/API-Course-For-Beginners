d3c7c4699f7b0e529d06b4499ca032c7688cfc31
John Doe <69224131+Samir-Gamal@users.noreply.github.com>
2021-09-04 15:35:45 +0300

    validation request response in postman

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -29,6 +30,15 @@ class PostController extends Controller
     }
 
     public function store(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|max:255',
+            'body' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->apiResponse(null,$validator->errors(),400);
+        }
 
 
         $post = Post::create($request->all());
